3b31184d1fd9d23c4895b492ff19018a4f197d95
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2026-01-02 18:00:21 +0600

    Add dotenv support: configure environment variables for datasource and JWT in application

--- src/main/java/com/safu/dev_registry/DevRegistryApplication.java
+++ src/main/java/com/safu/dev_registry/DevRegistryApplication.java
@@ -3,11 +3,21 @@ package com.safu.dev_registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class DevRegistryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevRegistryApplication.class, args);
-	}
+  public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.load();
+
+    System.setProperty("DATASOURCE_URL", dotenv.get("DATASOURCE_URL"));
+    System.setProperty("DATASOURCE_NAME", dotenv.get("DATASOURCE_NAME"));
+    System.setProperty("DATASOURCE_USERNAME", dotenv.get("DATASOURCE_USERNAME"));
+    System.setProperty("DATASOURCE_PASSWORD", dotenv.get("DATASOURCE_PASSWORD"));
+    System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+
+    SpringApplication.run(DevRegistryApplication.class, args);
+  }
 
 }
